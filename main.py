83a13c02d4ec102fa2902b7b1f9eb97cb082b0f6
Dov Cohen <idcohen@ymail.com>
2025-04-18 21:20:56 -0700

    updated at 21:21

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ log.set_labels('temperature')
 radio.config(group=23)
 radio.on()
 Filename = 'Temperature.txt'
+log.delete()
+#@run_every()
+
 while True:
     message = radio.receive()
     if message:
@@ -14,3 +17,15 @@ while True:
 #        append_data(Filename, message)
         log.add({'temperature':message})
         sleep(200)
+
+        @run_every(s=30)
+        def log_data():
+            log.add({
+              'temperature': temperature(),
+              'sound': microphone.sound_level(),
+              'light': display.read_light_level()
+            })
+            
+        while True:
+            sleep(100000)
+        
