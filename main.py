e6c775415e644dcf38bb549aefc60ce793e35728
Dov Cohen <idcohen@ymail.com>
2025-04-18 23:30:43 -0700

    20250418 EOD CI

--- main.py
+++ main.py
@@ -7,18 +7,36 @@ log.set_labels('temperature')
 radio.config(group=55)
 radio.on()
 
-Loop_ctrl = True
-while Loop_ctrl:
-    if button_b.was_pressed():
-        log.delete(full=True)
-        display.show('Clear')
+log.delete(full=True)
+display.show('Log Clear')
+display.clear()
 
-    message = radio.receive()
-    if message:
-#        print(str(message))
-        display.scroll(message)
-        log.add({'temperature':message})
+Loop_Ctrl_b = True
+while Loop_Ctrl_b:
+    Loop_Ctrl_a = True
+    if button_a.was_pressed():
+        while Loop_Ctrl_a:
+            message = radio.receive()
+            if message:
+        #        print(str(message))
+                display.scroll(message)
+                log.add({'temperature':message})
+            
+            if button_b.was_pressed():
+                Loop_Ctrl_a = False
+                Loop_Ctrl_b = False
+                radio.off()
+
+
         
-    if button_a.was_pressed():    
-        Loop_ctrl = False 
-        display.clear()
+        # @run_every(s=30)
+       # def log_data():
+        #    log.add({
+        #      'temperature': temperature(),
+        #      'sound': microphone.sound_level(),
+        #      'light': display.read_light_level()
+        #    })
+            
+        #while True:
+        #    sleep(100000)
+        
