101b106888ef16e4446fe9cabfecd065c0e217b5
Dov Cohen <idcohen@ymail.com>
2025-04-18 22:54:05 -0700

    2253 ci

--- main.py
+++ main.py
@@ -11,29 +11,14 @@ Loop_ctrl = True
 while Loop_ctrl:
     if button_b.was_pressed():
         log.delete(full=True)
-        display.scroll('Log Cleared')
+        display.show('Clear')
 
     message = radio.receive()
     if message:
-        print(str(message))
-        display.scroll(str(message))
+#        print(str(message))
+        display.scroll(message)
         log.add({'temperature':message})
         
     if button_a.was_pressed():    
-        Loop_ctrl = False   
-
-
-
-
-        
-        # @run_every(s=30)
-       # def log_data():
-        #    log.add({
-        #      'temperature': temperature(),
-        #      'sound': microphone.sound_level(),
-        #      'light': display.read_light_level()
-        #    })
-            
-        #while True:
-        #    sleep(100000)
-        
+        Loop_ctrl = False 
+        display.clear()
